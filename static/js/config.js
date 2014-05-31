07b22146529faba9285b07807d5e2064c939f6af
Mathew Davies <thepixeldeveloper@googlemail.com>
2014-05-31 17:43:04 +0100

    Update config

--- static/js/config.js
+++ static/js/config.js
@@ -11,7 +11,10 @@ requirejs.config({
         bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
         jquery: '../bower_components/jquery/dist/jquery',
         requirejs: '../bower_components/requirejs/require',
-        underscore: '../bower_components/underscore/underscore'
+        underscore: '../bower_components/underscore/underscore',
+        easeljs: '../bower_components/easeljs/lib/easeljs-0.7.1.combined',
+        jqueryui: '../bower_components/jqueryui/ui/jquery-ui',
+        mustache: '../bower_components/mustache/mustache'
     },
     packages: [
 
