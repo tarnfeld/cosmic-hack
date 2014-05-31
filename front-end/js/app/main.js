bfe312232240d754e7e4b9ffcb168d21e3c68ed9
Tom Arnfeld <tarnfeld@me.com>
2014-05-31 11:45:29 +0100

    Cleanup a bit

--- front-end/js/app/main.js
+++ front-end/js/app/main.js
@@ -1,8 +1,8 @@
 define(function (require) {
     var $ = require('jquery'),
         lib = require('./lib'),
-        controller = require('./controller/c2'),
-        model = require('./model/m2');
+        controller = require('./controller/main'),
+        model = require('./model/main');
 
     //A fabricated API to show interaction of
     //common and specific pieces.
