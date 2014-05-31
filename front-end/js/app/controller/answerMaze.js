6ad2db53ff0bb289efcda2195d8e94912d915c81
Mathew Davies <thepixeldeveloper@googlemail.com>
2014-05-31 14:01:36 +0100

    Generate thy maze

--- front-end/js/app/controller/answerMaze.js
+++ front-end/js/app/controller/answerMaze.js
@@ -1,7 +1,20 @@
-define(['./base'], function (base) {
+define([
+        './base',
+        '../../lib/maze/array',
+        '../../lib/maze/cell',
+        '../../lib/maze/graph',
+        '../../lib/maze/maze',
+        '../../lib/maze/mazeGenerator'
+    ],
+    function (base) {
     var controllerMaze = new base('Controller Maze');
 
     controllerMaze.render = function(dom) {
+
+        var maze = new Maze(document, 'maze');
+
+        maze.generate();
+        maze.draw();
 
     };
 
